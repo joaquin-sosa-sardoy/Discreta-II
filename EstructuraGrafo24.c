cbfee3d694ca051895619bb12617a2ff5023c270
Kurt_godel110 <andres.vecchiet@mi.un.edu.ar>
2024-03-15 20:48:33 -0300

    correccion kickstart, mejor version

--- EstructuraGrafo24.c
+++ EstructuraGrafo24.c
@@ -1,13 +1,7 @@
-#ifndef _LIST_
-#define _LIST_
 #include <stdio.h>
 #include <stdlib.h>             /* dynamic memory management*/
 #include <stdbool.h>            /* Definition of bool      */
 #include <assert.h>
+#include"EstructuraGrafo24.h"
 
-typedef int u32 ;
-typedef u32 color ;
-
-typedef struct s_vlist *vertex_list ; 
-
-#endif
+/* To do: implementar funciones de EstructuraGrafo24.h"*/
--- EstructuraGrafo24.h
+++ EstructuraGrafo24.h
@@ -4,12 +4,27 @@
 #include <stdlib.h>             /* dynamic memory management*/
 #include <stdbool.h>            /* Definition of bool      */
 #include <assert.h>
-#include "Vlist.h"
 
-struct GrafoSt {
-    vertex_list *neighbors; 
-    u32 vertex_num ;
-    u32 side_num ;
+typedef int u32 ;
+typedef u32 color ;
+
+typedef struct s_vlist *vertex_list ; 
+
+struct s_vertex{
+    u32 grado ;
+    color color ;
 };
 
+struct s_vlist {
+    struct s_vertex **vecinos; 
+    u32 length;
+}; 
+
+struct GrafoSt {
+    vertex_list *vertex; 
+    u32 vertex_num ;
+    u32 edges_num ;
+};
+
+/* TO DO:  agregar funciones para manipular lista con arreglos dinamicos*/
 #endif
