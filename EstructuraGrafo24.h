561f39a2dd9191958d581330fbef8de3f58d82cf
Jmmochkof <jose.mochkofsky@mi.unc.edu.ar>
2024-03-16 09:06:58 -0300

    cambio a la estructura de listas

--- EstructuraGrafo24.h
+++ EstructuraGrafo24.h
@@ -5,23 +5,26 @@
 #include <stdbool.h>            /* Definition of bool      */
 #include <assert.h>
 
-typedef int u32 ;
+typedef unsigned int u32 ;
 typedef u32 color ;
 
-typedef struct s_vlist *vertex_list ; 
+typedef struct s_list *list ;
+typedef struct s_vertex *vertex ;
+
+struct s_list {
+    u32 *chunk; //points to an array with the actual list elements
+    u32 length; //number of elements in the list
+    u32 allocd; //how much memory has been alloc´d to the list
+}; 
 
 struct s_vertex{
     u32 grado ;
     color color ;
+    list vecinos;
 };
 
-struct s_vlist {
-    struct s_vertex **vecinos; 
-    u32 length;
-}; 
-
 struct GrafoSt {
-    vertex_list *vertex; 
+    vertex *vertexes; //array of all the vertexes
     u32 vertex_num ;
     u32 edges_num ;
 };
